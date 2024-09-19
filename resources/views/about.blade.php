3fa3d8d3e3aa807713048f42a152f78cb0536ac0
Habeeb Bombata <bombatahabeeb@gmail.com>
2024-09-19 14:07:40 +0100

    laravel herd boilerplate easy

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+    <x-slot:heading>
+       About Page
+    </x-slot:heading>
+    <h1> About us </h1>
+    <p> Welcome to the about page </p>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+    <x-slot:heading>
+       Contact Page
+    </x-slot:heading>
+    <h1> Contact us</h1>
+    <p> Welcome to the Contact page </p>
+</x-layout>
